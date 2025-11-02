c7c736204a15d998cdd3b48ee44efe3090c79fbd
Divyansh0002-codes <divyanshsaini0002@gmail.com>
2025-11-02 21:12:39 +0530

    q2.c

---
+++ q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s,c;
+    printf("Enter two number : ");
+    scanf("%d%d",&a,&b);
+    
+        for(s=1;s<=a&&s<=b;s++)
+        {
+            if(a%d==0 && b%d==0)
+            c=s;
+        }
+         printf("HCF=%d",c);
+     return 0;
+}
